1d5385ee75a85ba628763dbfd262a14444a36c9d
User <User@DESKTOP-9R0KFMI>
2024-03-06 00:42:35 +0300

    Add project files.

---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,14 @@
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseAPIController : ControllerBase
+    {
+
+    }
+}
---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+   production: false,
+   apiUrl: 'https://localhost:58356/api/',
+   hubUrl: 'https://localhost:58356/hubs/'
+};
